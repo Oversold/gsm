88dc92cf96ceec457d49ae288e32d5e9b7ce2cff
Oversold <78127569+Oversold@users.noreply.github.com>
2023-01-06 14:40:39 -0500

    Update reqs.sh

--- reqs.sh
+++ reqs.sh
@@ -1,1 +1,2 @@
-sudo dpkg --add-architecture i386; sudo apt update; sudo apt install curl wget file tar bzip2 gzip unzip bsdmainutils python3 util-linux ca-certificates binutils bc jq tmux netcat lib32gcc-s1 lib32stdc++6 libsdl2-2.0-0:i386 steamcmd
+sudo dpkg --add-architecture i386
+sudo apt update; sudo apt install curl wget file tar bzip2 gzip unzip bsdmainutils python3 util-linux ca-certificates binutils bc jq tmux netcat lib32gcc-s1 lib32stdc++6 libsdl2-2.0-0:i386 steamcmd
